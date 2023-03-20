e3c4922718bca2557316787165a9835592f586f1
João Silvestre de Araújo <joao.s.araujo@gmail.com>
2023-03-20 00:51:00 -0300

    First Steps in Java With Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,15 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.erudio.Startup;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
